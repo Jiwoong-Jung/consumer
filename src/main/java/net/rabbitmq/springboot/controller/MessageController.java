c8b3b19a912f60de6955a531dc85f751b4482468
Jiwoong-Jung <jjw981119@naver.com>
2023-11-15 15:06:54 +0900

    Initial commit

---
+++ src/main/java/net/rabbitmq/springboot/controller/MessageController.java
@@ -0,0 +1,26 @@
+package net.rabbitmq.springboot.controller;
+
+
+import net.rabbitmq.springboot.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private RabbitMQProducer producer;
+
+    public MessageController(RabbitMQProducer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+        producer.sendMessage(message);
+        return ResponseEntity.ok("RabbitMQ로 메시지 보냄 ...");
+    }
+}
---
+++ src/main/java/net/rabbitmq/springboot/publisher/RabbitMQProducer.java
@@ -0,0 +1,27 @@
+package net.rabbitmq.springboot.publisher;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    public void sendMessage(String message) {
+        log.info("메시지 보냄: {}", message);
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
